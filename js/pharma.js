7e0ac1f01b7fdff34b23438c6d64eeed9448cf09
Showa <im.wont.it@gmail.com>
2018-05-23 01:06:54 +0500

    Big Changes, +AOS Js lib, MAin Js block, some fixes and responsibilities

---
+++ js/pharma.js
@@ -0,0 +1,8 @@
+$(document).load( AOS.init({
+    easing: 'ease-in-out-sine',
+    duration: 500,
+
+    /*{disable: function() {
+        var maxWidth = 800;
+        return window.innerWidth < maxWidth;}*/
+}));
